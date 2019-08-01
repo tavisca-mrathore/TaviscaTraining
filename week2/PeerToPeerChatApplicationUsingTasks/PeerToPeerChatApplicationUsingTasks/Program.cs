54101de6c0d0fe22a59098c2c498e9b5580202e6
Rathore <mrathore@internal.tavisca.com>
2019-08-01 16:55:34 +0530

    added code for p2p chat app and basic web server

---
+++ week2/PeerToPeerChatApplicationUsingTasks/PeerToPeerChatApplicationUsingTasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PeerToPeerChatApplicationUsingTasks
+{
+    class ConnectionCheckerTask
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
