bcfdceadea8fdf12d7492a8db51d9b0401dc9e60
Rathore <mrathore@internal.tavisca.com>
2019-09-04 17:58:22 +0530

    added sample functionality for displaying jquery and ajax

---
+++ week7/SampleWebAPI/WebApplication2/Controllers/SampleController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SampleWebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        public class EmployeeData
+        {
+            public string name;
+        }
+        // GET sample/
+        [HttpGet]
+        public List<EmployeeData> Get()
+        {
+            List<EmployeeData> emp_list_dictionary = new List<EmployeeData>(){
+                new EmployeeData(){ name = "generic employee name 1" },
+                new EmployeeData(){ name = "generic employee name 2" },
+                new EmployeeData(){ name = "generic employee name 3" },
+                new EmployeeData(){ name = "generic employee name 4" },
+                new EmployeeData(){ name = "generic employee name 5" }
+            };
+
+            return emp_list_dictionary;
+        }
+    }
+}
---
+++ week7/SampleWebAPI/WebApplication2/wwwroot/scripts/sampleJquery.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    let empListSelector = $("#emp-list");
+
+    $("#btn").click(function () {
+        if (empListSelector.children().length == 0) {
+            $.ajax({
+                type: 'GET',
+                url: 'sample/',
+                dataType: 'json',
+                success: function (data) {
+                    data.forEach(function (item) {
+                        let empLi = document.createElement("li");
+                        empLi.innerHTML = item["name"];
+                        empListSelector.append(empLi);
+                    });
+                    /*
+                    $.each(data, function (index) {
+                        if (data[index].hasOwnProperty("name")) {
+                            empListSelector.append(`<li>${data[index]["name"]}<li>`);
+                        }
+                    });
+                    */
+                },
+                error: function (result) {
+                    console.log("Error\t" + result);
+                }
+            });
+        }
+    });
+
+    $("#btnClear").click(function () {
+        empListSelector.empty();
+    });
+});
